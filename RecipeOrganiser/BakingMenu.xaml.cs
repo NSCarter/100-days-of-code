ad23913dd65f6311f72e3d375f69faeda3397ee6
Niamh Carter (cartern9) <cartern9@coventry.ac.uk>
2018-06-15 22:41:47 +0100

    Started the design

--- RecipeOrganiser/BakingMenu.xaml.cs
+++ RecipeOrganiser/BakingMenu.xaml.cs
@@ -24,5 +24,10 @@ namespace RecipeOrganiser
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
